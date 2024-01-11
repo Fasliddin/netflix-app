75eca6c932b4a42b65eaaa722fa2bbab559b5e28
Fasliddin <farhodjonovfasliddin71@gmail.com>
2024-01-11 11:54:21 +0500

    salom

---
+++ netflix/lib/screens/splash_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 5,
+        backgroundColor: Colors.yellow,
+      ),
+    );
+  }
+}
